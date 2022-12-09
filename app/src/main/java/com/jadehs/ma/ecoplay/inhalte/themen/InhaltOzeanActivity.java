8d332b7a384fec25deaca40508ffcac1ea789578
Nichlas Schipper <Nichlas12323@gmail.com>
2022-12-09 17:09:09 +0100

    ocean sticker

--- app/src/main/java/com/jadehs/ma/ecoplay/inhalte/themen/InhaltOzeanActivity.java
+++ app/src/main/java/com/jadehs/ma/ecoplay/inhalte/themen/InhaltOzeanActivity.java
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment;
 
 import com.jadehs.ma.ecoplay.R;
 import com.jadehs.ma.ecoplay.inhalte.InhaltActivity;
+import com.jadehs.ma.ecoplay.sticker.StickerManager;
 
 
 public class InhaltOzeanActivity extends InhaltActivity {
@@ -20,7 +21,9 @@ public class InhaltOzeanActivity extends InhaltActivity {
 
     @Override
     protected void onHasRead(long millisecondsSpent, long secondsSpent, long minutesSpent) {
-
+        if (secondsSpent >= 150 /* 2:30 min */) {
+            new StickerManager(this).unlockArchievement("text.ozean.1", this.getString(R.string.sticker_7_stickername));
+        }
     }
 
 }
--- app/src/main/java/com/jadehs/ma/ecoplay/quiz/quizzes/OzeanQuizActivity.java
+++ app/src/main/java/com/jadehs/ma/ecoplay/quiz/quizzes/OzeanQuizActivity.java
@@ -6,6 +6,7 @@ import androidx.annotation.Nullable;
 
 import com.jadehs.ma.ecoplay.R;
 import com.jadehs.ma.ecoplay.quiz.QuizActivity;
+import com.jadehs.ma.ecoplay.sticker.StickerManager;
 
 public class OzeanQuizActivity extends QuizActivity {
 
@@ -20,7 +21,12 @@ public class OzeanQuizActivity extends QuizActivity {
 
     @Override
     protected void onFinish(int correct, int count, double success) {
-
+        if (correct >= 5 && correct == count) {
+            new StickerManager(this).unlockArchievement("quiz.ozean.2", this.getString(R.string.sticker_9_stickername));
+        }
+        if (correct >= 3) {
+            new StickerManager(this).unlockArchievement("quiz.ozean.1", this.getString(R.string.sticker_10_stickername));
+        }
     }
 
 }

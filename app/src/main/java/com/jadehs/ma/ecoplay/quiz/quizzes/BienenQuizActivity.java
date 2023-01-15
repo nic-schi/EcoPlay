2642aae9273372de8bac453cf8989215890833a5
Nichlas Schipper <Nichlas12323@gmail.com>
2023-01-15 03:37:15 +0100

    bee quiz

--- app/src/main/java/com/jadehs/ma/ecoplay/quiz/quizzes/BienenQuizActivity.java
+++ app/src/main/java/com/jadehs/ma/ecoplay/quiz/quizzes/BienenQuizActivity.java
@@ -20,34 +20,34 @@ public class BienenQuizActivity extends QuizActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Frage 1: Bienenalter
+        // Frage 1
         FrageFragment frage1 = new FrageFragment(getString(R.string.quiz_1_frage_1_bezeichnung));
         frage1.addAntworten(getResources().getStringArray(R.array.quiz_1_frage_1_antworten));
-        frage1.setSolution(Frage.B);
+        frage1.setSolution(Frage.A);
         this.addFrage(frage1);
 
-        // Frage 2: Bienenalter
+        // Frage 2
         FrageFragment frage2 = new FrageFragment(getString(R.string.quiz_1_frage_2_bezeichnung));
         frage2.addAntworten(getResources().getStringArray(R.array.quiz_1_frage_2_antworten));
         frage2.setSolution(Frage.C);
         this.addFrage(frage2);
 
-        // Frage 3: Bienenalter
+        // Frage 3
         FrageFragment frage3 = new FrageFragment(getString(R.string.quiz_1_frage_3_bezeichnung));
         frage3.addAntworten(getResources().getStringArray(R.array.quiz_1_frage_3_antworten));
-        frage3.setSolution(Frage.A);
+        frage3.setSolution(Frage.D);
         this.addFrage(frage3);
 
-        // Frage 4: Bienenalter
+        // Frage 4
         FrageFragment frage4 = new FrageFragment(getString(R.string.quiz_1_frage_4_bezeichnung));
         frage4.addAntworten(getResources().getStringArray(R.array.quiz_1_frage_4_antworten));
-        frage4.setSolution(Frage.A);
+        frage4.setSolution(Frage.B);
         this.addFrage(frage4);
 
-        // Frage 5: Bienenalter
+        // Frage 5
         FrageFragment frage5 = new FrageFragment(getString(R.string.quiz_1_frage_5_bezeichnung));
         frage5.addAntworten(getResources().getStringArray(R.array.quiz_1_frage_5_antworten));
-        frage5.setSolution(Frage.D);
+        frage5.setSolution(Frage.A);
         this.addFrage(frage5);
     }
 
